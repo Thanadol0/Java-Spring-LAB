3c8478b500ddbf6a378b8586037e4f1ff8d98c76
Thanadol0 <141946404+Thanadol0@users.noreply.github.com>
2025-05-19 23:32:21 +0700

    Start Keycloak LAB5 (MFA with Email-OTP)

---
+++ Keycloak-Basic/Keycloak-LAB5(MFA-with-Email-OTP)/src/test/java/demo/Keycloak_LAB5/KeycloakLab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.Keycloak_LAB5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakLab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
