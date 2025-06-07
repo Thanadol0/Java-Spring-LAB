ac52ed061089a58294208a9f52ec860a0e59e53a
Thanadol0 <141946404+Thanadol0@users.noreply.github.com>
2025-06-07 23:58:25 +0700

    remove keycloak LAB6(CIBA) And Add Grafana-basic LAB

---
+++ Grafana-Basic/services/banking-service-test/src/main/java/demo/banking_service_test/BankingServiceTestApplication.java
@@ -0,0 +1,13 @@
+package demo.banking_service_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingServiceTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingServiceTestApplication.class, args);
+	}
+
+}
---
+++ Grafana-Basic/services/banking-service-test/src/test/java/demo/banking_service_test/BankingServiceTestApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.banking_service_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingServiceTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
